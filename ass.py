0fe64f94a69d069025f466f29abfad1fd32dd627
eslam mohamed <44472958+eslam-mohamed471@users.noreply.github.com>
2020-03-25 23:31:41 +0200

    Create ass.py

---
+++ ass.py
@@ -0,0 +1,20 @@
+from hello import rearrange
+import unittest
+class Testrea(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Eslam, mohamed"
+        expected = ("mohamed Eslam")
+        self.assertEqual(rearrange(testcase),expected)
+    def test_empty(self):
+        testcase = ""
+        expected = ("")
+        self.assertEqual(rearrange(testcase),expected)
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange(testcase),expected)
+    def test_one_name(self):
+        testcase = "Hopper"
+        expected = "Hopper"
+        self.assertEqual(rearrange(testcase),expected)
+unittest.main()
